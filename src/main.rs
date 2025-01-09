d8722239bc24aa1bd3163c71847dab760b3172c5
Gergő Csókás <gergo.csokas@gmail.com>
2025-01-09 21:10:24 +0700

    :construction: [Project] Adding workspace dependencies to the main `Cargo.toml` and modifying the `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-use axum::Router;
-use axum::routing::get;
-
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
-
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+fn main() {
+    panic!("Please run the `main` crate instead.");
 }

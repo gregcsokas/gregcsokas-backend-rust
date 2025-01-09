de95c854c70f4bc28fb3970860e584e084781603
Gergő Csókás <gergo.csokas@gmail.com>
2025-01-09 20:43:30 +0700

    :sparkles: [Project] Integrate `axum` and "Hello world" functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }

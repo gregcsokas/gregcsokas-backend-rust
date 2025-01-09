ae9e130ada09fb32df5d4fe257c5ffa342d055cf
Gergő Csókás <gergo.csokas@gmail.com>
2025-01-09 21:00:20 +0700

    :sparkles: [Core] Create `main` app as a workspace member.

---
+++ main/src/main.rs
@@ -0,0 +1,10 @@
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}

1a97be4fcb0bb3d6156e49b58a94eea208fb43bf
Gergő Csókás <gergo.csokas@gmail.com>
2025-01-09 21:28:01 +0700

    :sparkles: [Project] Adding `newsletter` lib to the main application.

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,9 +1,15 @@
 use axum::Router;
 use axum::routing::get;
+use newsletter::newsletter_routes;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let newsletter_app = newsletter_routes();
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, world!" }))
+        .nest("/newsletter", newsletter_app);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
